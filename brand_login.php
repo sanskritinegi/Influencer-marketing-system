1accd05ce1b2171bcb0eb5feaa9632a72218eb06
Prateek Devashetti <62307251+PrateekDevashetti@users.noreply.github.com>
2021-03-24 08:33:43 +0530

    Create brand_login.php

---
+++ brand_login.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<title>User registration system using php and mySQL</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body background="grey.png" style="background-size: cover;">
+<div class="header"> <h2>LOGIN</h2> </div>
+<form name="log" action="brand_valid.php" method="POST">
+<div class="input-group">
+<label>Username</label>
+<input type="text" name="username"> </div>
+<div class="input-group">
+<label>Password</label>
+<input type="password" name="password"> </div>
+<div class="input-group">
+<button type="submit" name="submit" class="btn">Submit</button> </div>
+</form> </body>
+</html>
